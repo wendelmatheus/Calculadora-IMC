942dfc9042b37d66f8795f9b94d97c153531d0d1
wendelmatheus <wendeltel6a@gmail.com>
2022-12-20 04:19:06 -0300

    Atualização Códigos

--- CalculadoraIMC/assets/script.js
+++ CalculadoraIMC/assets/script.js
@@ -1,5 +1,4 @@
 const calcular = document.querySelector("input#botaoCalcular")
-// calcular.addEventListener('click', calcularImc)
 
 function calcularImc() {
     const nome = document.querySelector("input#nomeUsuario").value
@@ -7,22 +6,29 @@ function calcularImc() {
     const peso = document.querySelector("input#pesoUsuario").value
     const resultado = document.querySelector("div#resultado")   
     
+    // se o usuário preencheu todos os dados
     if(nome !== '' && altura !== '' && peso !== '') {
         const imc = peso / (altura ^ 2)
         if(imc < 18.5) {
+            // se o usuário estiver abaixo do peso
             resultado.textContent = `${nome}, seu IMC é ${imc.toFixed(2)} e você está abaixo do peso!`
         } else if(imc >= 18.5 && imc < 25) {
+            // se o usuário estiver no peso ideal
             resultado.textContent = `${nome}, seu IMC é ${imc.toFixed(2)} e você está com o peso ideal. Parabéns!`
         } else if(imc >= 25 && imc < 30) {
+            // se o usuário estiver acima do peso
             resultado.textContent = `${nome}, seu IMC é ${imc.toFixed(2)} e você está acima do peso!`
         } else if(imc >= 30 && imc < 35) {
+            // se o usuário estiver com obesidade
             resultado.textContent = `${nome}, seu IMC é ${imc.toFixed(2)} e você está com obesidade!`
         } else if(imc >= 35) {
+            // se o usuário estiver com obesidade extrema
             resultado.textContent = `${nome}, seu IMC é ${imc.toFixed(2)} e você está com obesidade extrema!`
         } else {
             resultado.textContent = '[ERRO]'
         }
     } else {
+        // se o usuário não preencheu todos os dados
         resultado.textContent = '[ERRO] Você não preencheu todos os dados!'
     }
 }
